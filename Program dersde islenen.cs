3c17c6ab86f4c52948214d2e82d733b2b5b5231f
HidanYugakure <fidants@code.edu.az>
2023-10-17 22:43:42 +0400

    dersdeki tasklar

---
+++ Program dersde islenen.cs
@@ -0,0 +1,62 @@
+﻿
+#region task1
+//int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+//int result = 1;
+
+//foreach (int numbers in num) 
+//{
+//    if (numbers > 0 && numbers<20)
+//    {
+//        result *= numbers;
+//    }
+//}
+//Console.WriteLine(result);
+#endregion
+#region task2
+//int[] num = { 1, 3, 8, 15, 98 };
+//int result = 0;
+//foreach(int numbers in num)
+//{
+//    if (numbers % 2 == 0) 
+//    {
+//        result += numbers;
+//    }
+
+//}
+//Console.WriteLine(result*result);
+#endregion
+
+#region task3
+
+//int n = 35;
+//int m = 48;
+//int result = 0;
+
+//if (n % 2 == 0 && m % 2 == 0)
+//{
+//    result += (n + m);
+//    Console.WriteLine(result);
+//}
+//else 
+//{
+//    Console.WriteLine("cutededdeyil");
+//}
+
+
+
+#endregion
+
+#region task 4
+
+//int m = 8;
+//int faktorial = 1;
+//for (int i = 1; i <=m; i++)
+//{
+//    faktorial=faktorial*i;
+//}
+//Console.WriteLine(faktorial);
+
+#endregion
+
+
+
